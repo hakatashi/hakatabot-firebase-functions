960f02a388f08a97005be60ff11ba79466d79f08
Koki Takahashi <hakatasiloving@gmail.com>
2020-11-04 06:31:11 +0900

    Implement few functions to authenticate google users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,40 @@
-import {https, logger} from 'firebase-functions';
+import {https, logger, config as getConfig} from 'firebase-functions';
 import {google} from 'googleapis';
 
 export * from './slack';
 
 const fitness = google.fitness('v1');
 
-export const helloWorld = https.onRequest(async (request, response) => {
-	const dataSources = await fitness.users.dataSources.list({userId: 'me'});
-	logger.info(dataSources, {structuredData: true});
-	response.send(JSON.stringify(dataSources, null, '  '));
+const config = getConfig();
+
+const oauth2Client = new google.auth.OAuth2(
+	config.google.client_id,
+	config.google.client_secret,
+	'https://us-central1-hakatabot-firebase-functions.cloudfunctions.net/googleApiOauthCallback',
+);
+
+export const authenticateGoogleApi = https.onRequest((request, response) => {
+	const url = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+
+		scope: ['https://www.googleapis.com/auth/fitness.activity.read'],
+	});
+	logger.info(url);
+	response.redirect(url);
+});
+
+export const googleApiOauthCallback = https.onRequest(async (request, response) => {
+	const code = request.query?.code;
+	if (!code || typeof code !== 'string') {
+		response.sendStatus(400).end();
+		return;
+	}
+	const {tokens} = await oauth2Client.getToken(code);
+	oauth2Client.setCredentials(tokens);
+	const data = await fitness.users.sessions.list({
+		userId: 'me',
+		auth: oauth2Client,
+	});
+	logger.info(data);
+	response.send('hoge');
 });
