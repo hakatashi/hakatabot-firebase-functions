112d7e078a72e8cb5a5db4adb9cf237c19634d71
Koki Takahashi <hakatasiloving@gmail.com>
2020-12-11 01:03:05 +0900

    Implement food photo sharing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,15 @@
+import {IncomingWebhook} from '@slack/webhook';
+import axios from 'axios';
 import {https, logger, config as getConfig} from 'firebase-functions';
 import {google} from 'googleapis';
 
 export * from './slack';
 
-const fitness = google.fitness('v1');
+// const fitness = google.fitness('v1');
 
 const config = getConfig();
+
+const cookingWebhook = new IncomingWebhook(config.slack.webhooks.cooking);
 
 const oauth2Client = new google.auth.OAuth2(
 	config.google.client_id,
@@ -16,8 +20,11 @@ const oauth2Client = new google.auth.OAuth2(
 export const authenticateGoogleApi = https.onRequest((request, response) => {
 	const url = oauth2Client.generateAuthUrl({
 		access_type: 'offline',
-
-		scope: ['https://www.googleapis.com/auth/fitness.activity.read'],
+		scope: [
+			'https://www.googleapis.com/auth/photoslibrary',
+			'https://www.googleapis.com/auth/photoslibrary.readonly',
+			'https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddata',
+		],
 	});
 	logger.info(url);
 	response.redirect(url);
@@ -31,10 +38,45 @@ export const googleApiOauthCallback = https.onRequest(async (request, response) 
 	}
 	const {tokens} = await oauth2Client.getToken(code);
 	oauth2Client.setCredentials(tokens);
-	const data = await fitness.users.sessions.list({
-		userId: 'me',
-		auth: oauth2Client,
+
+	const res = await axios.post('https://photoslibrary.googleapis.com/v1/mediaItems:search', {
+		filters: {
+			contentFilter: {
+				includedContentCategories: [
+					'FOOD',
+				],
+			},
+		},
+	}, {
+		headers: {
+			Authorization: `Bearer ${tokens.access_token}`,
+			'Content-Type': 'application/json',
+		},
 	});
-	logger.info(data);
+
+	const item = res.data.mediaItems[4];
+	const url = `${item.baseUrl}=w1264-h948`;
+	await cookingWebhook.send({
+		text: `料理した <${url}|写真>`,
+		unfurl_links: false,
+		unfurl_media: false,
+		blocks: [
+			{
+				type: 'section',
+				text: {
+					type: 'plain_text',
+					text: '料理した',
+					emoji: true,
+				},
+			},
+			{
+				type: 'image',
+				block_id: 'image',
+				image_url: url,
+				alt_text: '料理',
+			},
+		],
+	});
+
 	response.send('hoge');
 });
