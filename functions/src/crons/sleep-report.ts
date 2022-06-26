1672fe565522c2568c456509cf7c117d73786e97
Koki Takahashi <hakatasiloving@gmail.com>
2022-06-26 20:23:19 +0900

    Implement asa message auto-posting with fitbot sleep logs

--- functions/src/crons/sleep-report.ts
+++ functions/src/crons/sleep-report.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import {logger, pubsub} from 'firebase-functions';
-import {EXPIRATION_WINDOW_IN_SECONDS, HAKATASHI_EMAIL} from '../const';
+import {get} from 'lodash';
+import {EXPIRATION_WINDOW_IN_SECONDS, HAKATASHI_EMAIL, SANDBOX_ID} from '../const';
 import {FitbitSleeps, FitbitTokens} from '../firestore';
 import {client} from '../fitbit';
+import {webClient as slack} from '../slack';
 
 export const sleepGetCronJob = pubsub.schedule('every 5 minutes').onRun(async () => {
 	const hakatashiTokensData = await FitbitTokens.doc(HAKATASHI_EMAIL).get();
@@ -38,6 +40,46 @@ export const sleepGetCronJob = pubsub.schedule('every 5 minutes').onRun(async ()
 
 	logger.info(`Retrieved ${res.data.sleep.length} sleeps`);
 	for (const sleep of res.data.sleep) {
-		await FitbitSleeps.doc(sleep.logId.toString()).set(sleep, {merge: true});
+		const key = sleep.logId.toString();
+		const doc = await FitbitSleeps.doc(key).get();
+		if (!doc.exists) {
+			const wake = get(sleep, ['levels', 'summary', 'wake', 'minutes'], 0);
+			const rem = get(sleep, ['levels', 'summary', 'rem', 'minutes'], 0);
+			const light = get(sleep, ['levels', 'summary', 'light', 'minutes'], 0);
+			const deep = get(sleep, ['levels', 'summary', 'deep', 'minutes'], 0);
+
+			const imageUrl = new URL(`https://image-charts.com/chart?${new URLSearchParams({
+				chbr: '0',
+				chco: 'e73360,7ec4ff,3f8dff,154ba6',
+				chd: `a:${wake}|${rem}|${light}|${deep}`,
+				chdlp: 'r',
+				chma: '-100,-100,-100,-100',
+				chs: '700x10',
+				cht: 'bhs',
+			})}`);
+
+			await slack.chat.postMessage({
+				as_user: true,
+				channel: SANDBOX_ID,
+				text: 'あさ！',
+				blocks: [
+					{
+						type: 'image',
+						block_id: 'image',
+						image_url: imageUrl.toString(),
+						alt_text: `睡眠統計 (覚醒: ${wake}分, レム睡眠: ${rem}分, 浅い睡眠: ${light}分, 深い睡眠: ${deep}分)`,
+					},
+					{
+						type: 'section',
+						text: {
+							type: 'plain_text',
+							text: 'あさ！',
+							emoji: true,
+						},
+					},
+				],
+			});
+		}
+		await FitbitSleeps.doc(key).set(sleep, {merge: true});
 	}
 });
