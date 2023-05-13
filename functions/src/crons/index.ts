5c7669589e24e412bb57a96191161accb5dfbadd
Koki Takahashi <hakatasiloving@gmail.com>
2023-05-13 15:14:44 +0900

    Remove foodshare cron jobs

--- functions/src/crons/index.ts
+++ functions/src/crons/index.ts
@@ -1,7 +1,5 @@
-export * from './foodshare';
 export * from './wordblog';
 export * from './status-changer';
-export * from './glyphwikibot';
 export * from './exercise-report';
 export * from './sleep-report';
 export * from './sleep-battle';
