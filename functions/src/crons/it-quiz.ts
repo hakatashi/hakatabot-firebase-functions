cd54d1bf09d112f51cb377ae9c88d349733dfe3d
Koki Takahashi <hakatasiloving@gmail.com>
2024-02-20 20:34:02 +0000

    ♻️ (it-quiz.ts): refactor code to use State class from firestore.js for improved     maintainability and readability   ✨ (it-quiz.ts): introduce new function getItQuizStatsImageUrl to fetch stats image     URL based on timestamp   ✨ (it-quiz.ts): introduce new itQuizMilestoneProgressCronJob to handle     milestone progress updates for IT quiz with related Slack notifications and     image charts interpolation

--- functions/src/crons/it-quiz.ts
+++ functions/src/crons/it-quiz.ts
@@ -1,13 +1,15 @@
 import type {ChartConfiguration} from 'chart.js';
-import dayjs from 'dayjs';
+import dayjs, {Dayjs} from 'dayjs';
 import {logger, pubsub} from 'firebase-functions';
 import {google} from 'googleapis';
 import {IT_QUIZ_GOOGLE_SHEET_ID, IT_QUIZ_ID} from '../const.js';
-import {ItQuizProgressStats} from '../firestore.js';
+import {ItQuizProgressStats, State} from '../firestore.js';
 import {getGoogleAuth} from '../google.js';
 import {webClient as slack} from '../slack.js';
 
 const weekdayLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const state = new State('it-quiz');
 
 const getItQuizStats = async () => {
 	const auth = await getGoogleAuth();
@@ -34,6 +36,80 @@ const getItQuizStats = async () => {
 	return {done, ideas};
 };
 
+const getItQuizStatsImageUrl = async (timestamp: Dayjs) => {
+	logger.info('itQuizProgressCronJob: Getting stats of past 2 weeks...');
+
+	const dates = Array(14).fill(0).map((_, index) => {
+		const date = timestamp.subtract(13 - index, 'day').format('YYYY-MM-DD');
+		return date;
+	});
+
+	const twoWeeksAgoDate = timestamp.subtract(2, 'week').format('YYYY-MM-DD');
+	const results = await ItQuizProgressStats
+		.where('date', '>=', twoWeeksAgoDate)
+		.orderBy('date', 'asc')
+		.get();
+
+	const stats = results.docs.map((doc) => doc.data());
+	logger.info(`itQuizProgressCronJob: stats = ${JSON.stringify(stats)}`);
+
+	const imageChartsPayload: ChartConfiguration = {
+		type: 'line',
+		data: {
+			labels: dates.map((date) => {
+				const weekday = dayjs(date).day();
+				return weekdayLabels[weekday];
+			}),
+			datasets: [
+				{
+					label: 'Ideas',
+					backgroundColor: '#FF6384',
+					borderColor: '#FF6384',
+					data: dates.map((date) => {
+						const stat = stats.find((s) => s.date === date);
+						return stat?.ideas ?? null;
+					}),
+					fill: false,
+				},
+				{
+					label: 'Done',
+					backgroundColor: '#36A2EB',
+					borderColor: '#36A2EB',
+					data: dates.map((date) => {
+						const stat = stats.find((s) => s.date === date);
+						return stat?.done ?? null;
+					}),
+					fill: false,
+				},
+				{
+					label: 'Target',
+					borderWidth: 1,
+					borderColor: '#FF9800',
+					data: dates.map(() => 30),
+					pointRadius: 0,
+					fill: false,
+				},
+			],
+		},
+		options: {
+			title: {
+				display: true,
+				text: 'IT quiz stocks',
+			},
+		},
+	};
+
+	const imageChartsUrl = `https://image-charts.com/chart.js/2.8.0?${new URLSearchParams({
+		bkg: 'white',
+		c: JSON.stringify(imageChartsPayload),
+		width: '500',
+		height: '300',
+	})}`;
+	logger.info(`itQuizProgressCronJob: imageChartsUrl = ${imageChartsUrl}`);
+
+	return imageChartsUrl;
+};
+
 export const itQuizProgressCronJob = pubsub
 	.schedule('0 19 * * *')
 	.timeZone('Asia/Tokyo')
@@ -51,75 +127,7 @@ export const itQuizProgressCronJob = pubsub
 			date: currentDate,
 		}, {merge: true});
 
-		logger.info('itQuizProgressCronJob: Getting stats of past 2 weeks...');
-
-		const dates = Array(14).fill(0).map((_, index) => {
-			const date = timestamp.subtract(13 - index, 'day').format('YYYY-MM-DD');
-			return date;
-		});
-
-		const twoWeeksAgoDate = timestamp.subtract(2, 'week').format('YYYY-MM-DD');
-		const results = await ItQuizProgressStats
-			.where('date', '>=', twoWeeksAgoDate)
-			.orderBy('date', 'asc')
-			.get();
-
-		const stats = results.docs.map((doc) => doc.data());
-		logger.info(`itQuizProgressCronJob: stats = ${JSON.stringify(stats)}`);
-
-		const imageChartsPayload: ChartConfiguration = {
-			type: 'line',
-			data: {
-				labels: dates.map((date) => {
-					const weekday = dayjs(date).day();
-					return weekdayLabels[weekday];
-				}),
-				datasets: [
-					{
-						label: 'Ideas',
-						backgroundColor: '#FF6384',
-						borderColor: '#FF6384',
-						data: dates.map((date) => {
-							const stat = stats.find((s) => s.date === date);
-							return stat?.ideas ?? null;
-						}),
-						fill: false,
-					},
-					{
-						label: 'Done',
-						backgroundColor: '#36A2EB',
-						borderColor: '#36A2EB',
-						data: dates.map((date) => {
-							const stat = stats.find((s) => s.date === date);
-							return stat?.done ?? null;
-						}),
-						fill: false,
-					},
-					{
-						label: 'Target',
-						borderWidth: 1,
-						borderColor: '#FF9800',
-						data: dates.map(() => 30),
-						pointRadius: 0,
-						fill: false,
-					},
-				],
-			},
-			options: {
-				title: {
-					display: true,
-					text: 'IT quiz stocks',
-				},
-			},
-		};
-
-		const imageChartsUrl = `https://image-charts.com/chart.js/2.8.0?${new URLSearchParams({
-			bkg: 'white',
-			c: JSON.stringify(imageChartsPayload),
-			width: '500',
-			height: '300',
-		})}`;
-		logger.info(`itQuizProgressCronJob: imageChartsUrl = ${imageChartsUrl}`);
+		const imageChartsUrl = await getItQuizStatsImageUrl(timestamp);
 
 		const slackText = `【ITクイズの現在の進捗】\n完了: ＊${done}問＊ / アイデア: ＊${ideas}問＊`;
 		const slackMessage = await slack.chat.postMessage({
@@ -144,4 +152,67 @@ export const itQuizProgressCronJob = pubsub
 		});
 
 		logger.info(`itQuizProgressCronJob: slackMessage = ${JSON.stringify(slackMessage)}`);
+	});
+
+export const itQuizMilestoneProgressCronJob = pubsub
+	.schedule('every 10 minutes')
+	.timeZone('Asia/Tokyo')
+	.onRun(async (context) => {
+		const timestamp = dayjs(context.timestamp).tz('Asia/Tokyo');
+		const currentDate = timestamp.format('YYYY-MM-DD');
+		logger.info(`itQuizMilestoneProgressCronJob triggered at ${timestamp} (date = ${currentDate})`);
+
+		const previousProgress = await state.get('previousProgress', 0);
+		const {done, ideas} = await getItQuizStats();
+
+		logger.info(`itQuizMilestoneProgressCronJob: done = ${done}, ideas = ${ideas}, previousProgress = ${previousProgress}`);
+
+		await state.set({previousProgress: done});
+
+		let milestoneCompleted: null | number = null;
+		if (done > previousProgress) {
+			if (done >= 10 && previousProgress < 10) {
+				milestoneCompleted = 10;
+			}
+			if (done >= 20 && previousProgress < 20) {
+				milestoneCompleted = 20;
+			}
+			if (done >= 30 && previousProgress < 30) {
+				milestoneCompleted = 30;
+			}
+		}
+
+		if (milestoneCompleted === null) {
+			return;
+		}
+
+		await ItQuizProgressStats.doc(currentDate).set({
+			done,
+			ideas,
+			date: currentDate,
+		}, {merge: true});
+
+		const imageChartsUrl = await getItQuizStatsImageUrl(timestamp);
+
+		const slackText = `【ITクイズの現在の進捗 (${milestoneCompleted}問突破)】\n完了: ＊${done}問＊ / アイデア: ＊${ideas}問＊`;
+		await slack.chat.postMessage({
+			channel: IT_QUIZ_ID,
+			username: 'ITクイズ進捗くん',
+			icon_emoji: ':quora:',
+			text: slackText,
+			blocks: [
+				{
+					type: 'section',
+					text: {
+						type: 'mrkdwn',
+						text: slackText,
+					},
+				},
+				{
+					type: 'image',
+					image_url: imageChartsUrl,
+					alt_text: 'IT quiz stocks',
+				},
+			],
+		});
 	});
