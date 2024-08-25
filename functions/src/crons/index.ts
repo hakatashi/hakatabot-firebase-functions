d89af18db2a4ba87a1cf1076a730491ff84c111e
Koki Takahashi <hakatasiloving@gmail.com>
2024-08-25 21:36:52 +0900

    Remove sleepMorningReportCronJob

--- functions/src/crons/index.ts
+++ functions/src/crons/index.ts
@@ -3,7 +3,6 @@ export * from './status-changer.js';
 export * from './exercise-report.js';
 export * from './sleep-report.js';
 export * from './sleep-battle.js';
-export * from './morning-report.js';
 export * from './rinna-ping.js';
 export * from './it-quiz.js';
 export * from './tsgctf-platform.js';
